3f05dd65716c3795fa953b53a0436eebcbbf6bfd
Lisandro Torresetti <lisandrotorresetti@gmail.com>
2025-09-21 23:06:32 -0300

    feat add debounce header file for exercise 2

---
+++ practica_4_ej_2/Drivers/API/Inc/API_debounce.h
@@ -0,0 +1,14 @@
+#ifndef API_INC_API_DEBOUNCE_H_
+#define API_INC_API_DEBOUNCE_H_
+
+#include <stdbool.h>
+
+void debounceFSM_init();
+
+void debounceFSM_update();
+
+bool readKey();
+
+
+
+#endif /* API_INC_API_DEBOUNCE_H_ */
